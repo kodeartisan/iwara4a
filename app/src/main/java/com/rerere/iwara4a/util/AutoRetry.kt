c0495763c1685f36761af0e17affbc8a5ef9ef43
jiangdashao <qq1609403959@outlook.com>
2021-06-07 16:32:35 +0800

    自动重试函数注释 😅

--- app/src/main/java/com/rerere/iwara4a/util/AutoRetry.kt
+++ app/src/main/java/com/rerere/iwara4a/util/AutoRetry.kt
@@ -6,9 +6,16 @@ import com.rerere.iwara4a.api.Response
 
 private const val TAG = "AutoRetry"
 
+/**
+ * 自动重试函数, 用于iwara的服务器大概率会无响应，因此需要尝试多次才能获取到响应内容
+ *
+ * @param maxRetry 重试次数
+ * @param action 重试体
+ * @return 最终响应
+ */
 suspend fun <T> autoRetry(@IntRange(from = 2) maxRetry: Int = 5, action: suspend () -> Response<T>): Response<T> {
     repeat(maxRetry - 1) {
-        Log.i(TAG, "autoRetry: try to get response: ${it + 1}/$maxRetry")
+        Log.i(TAG, "autoRetry: Try to get response: ${it + 1}/$maxRetry")
         val start = System.currentTimeMillis()
         val response = action()
         if (response.isSuccess()) {
@@ -16,6 +23,6 @@ suspend fun <T> autoRetry(@IntRange(from = 2) maxRetry: Int = 5, action: suspend
             return response
         }
     }
-    Log.i(TAG, "autoRetry: try to get response: last/$maxRetry")
+    Log.i(TAG, "autoRetry: Try to get response: $maxRetry*/$maxRetry")
     return action()
 }
